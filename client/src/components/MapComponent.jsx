2ccfaaa561fcf2961018ce78f3ce2603e877e833
shashi1273 <shashi_1273@yahoo.com>
2025-04-29 10:58:01 -0400

    Update MapComponent.jsx

--- client/src/components/MapComponent.jsx
+++ client/src/components/MapComponent.jsx
@@ -13,7 +13,7 @@ function MapComponent({ onMapClick }) {
       }}
       style={{ width: '100vw', height: '400px' }}
       mapStyle="mapbox://styles/mapbox/streets-v11"
-      onClick={(e) => onMapClick(e.lngLat)}
+      onClick={(e) => onMapClick({ lat: e.lngLat.lat, lng: e.lngLat.lng })}  
     />
   );
 }
