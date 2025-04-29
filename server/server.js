6fc92aeb3694607c1cdad58226d7a3c9fa72f269
shashi1273 <shashi_1273@yahoo.com>
2025-04-29 10:33:45 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -94,6 +94,7 @@ io.on('connection', socket => {
   });
 });
 
-server.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
+server.listen(process.env.PORT || 3001, () => {
+  console.log(`Server running on port ${process.env.PORT || 3001}`);
 });
+
